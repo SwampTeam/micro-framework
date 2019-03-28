a5413cabaad0e59cafebac04ec0976027731385f
adolfo <buylla.adolfo@hotmail.com>
2019-03-28 15:45:26 +0100

    read file need css :)

--- public/read.php
+++ public/read.php
@@ -1,1 +1,22 @@
 <?php
+require_once __DIR__ . '/../src/bootstrap.php';
+
+use Model\ParkPlace;
+
+$parkPlaces = ParkPlace::findAll();
+
+//echo($park['id']);
+foreach ($parkPlaces as $parkPlace) { ?>
+<table>
+    <b><?php echo $parkPlace->getType(); ?></b>
+    <?php echo $parkPlace->getNumber(); ?>
+    <?php
+    if ($parkPlace->isOccupied() == 0) {
+        echo "<span style='color:green'>Parking is free</span>";
+    }else{
+        echo "<span style='color:red'>Parking is in use</span>";
+    } ?>
+    <?php
+    }
+    ?>
+</table>
